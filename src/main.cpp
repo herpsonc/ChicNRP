bf134a8689ad1dac01a13dfb2413fde6aabee234
BassemYagoub <yagoub.bassem@gmail.com>
2020-05-02 17:16:15 +0200

    Ajout du service SDC (Mars) pour les tests

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,13 @@
 #include "model/constraint/ConstraintSeqMinMax.h"
 using namespace std;
 
+void addConsecutiveSamePost(Agent* a, Post* p, int d_start, int d_end) {
+	//permet d'ajouter le même post pendant d_end-d_start jours d'affilé à un agent
+	for (d_start; d_start <= d_end; d_start++) {
+		a->setCalendarDay(p, d_start, true);
+	}
+}
+
 Model generateGhr() {
 
 		//Creation du model pour le service GHR
@@ -183,13 +190,190 @@ Model generateGhr() {
 
 }
 
+Model addServiceSDC(Model m) {
+
+	//Ajout du service SDC à un modèle existant
+
+	Service* sdc = new Service("SDC");
+
+	m.addService(sdc);
+
+	//Posts
+	Post* js = new Post("Js", 12.25);
+	js->addAttribut("workL");
+	js->addAttribut("work");
+	js->addAttribut("day");
+	Post* ns = new Post("Js", 12.25);
+	ns->addAttribut("workL");
+	ns->addAttribut("work");
+	ns->addAttribut("night");
+
+	Post* uk = new Post("UK", 12.25);
+	uk->addAttribut("workL");
+	uk->addAttribut("work");
+	uk->addAttribut("day");
+
+
+	Post* repos = new Post("Repos", 0.0);
+	repos->addAttribut("rest");
+	Post* ca = new Post("Ca", 0.0);
+	ca->addAttribut("rest");
+	ca->addAttribut("ca");
+
+	Post* fp = new Post("FP", 7.5);
+	fp->addAttribut("work");
+	fp->addAttribut("day");
+
+	Post* cs = new Post("CS", 7.5);
+	cs->addAttribut("work");
+	cs->addAttribut("day");
+
+	m.setDefaultPost(repos);
+
+	sdc->addPost(js);
+	sdc->addPost(ns);
+	sdc->addPost(uk);
+	sdc->addPost(repos);
+	//ghr->addPost(ca);
+
+	sdc->addPostRequired(js, 3);
+	sdc->addPostRequired(ns, 1);
+	sdc->addPostRequired(uk, 1);
+
+
+	//Contraintes
+
+	//Pas de Nuit/Jour
+	auto v = vector<string>();
+	v.push_back("night");
+	v.push_back("day");
+	ConstraintDaysSeq* cJN = new ConstraintDaysSeq(v, 1);
+
+	//Pas 3 jours/nuit de travail d'affilé
+	v = vector<string>();
+	v.push_back("workL");
+	v.push_back("workL");
+	v.push_back("workL");
+	ConstraintDaysSeq* c3N = new ConstraintDaysSeq(v, 1);
+
+	//Pas de nuit avant un congé posé
+	v = vector<string>();
+	v.push_back("night");
+	v.push_back("ca");
+	ConstraintDaysSeq* crn = new ConstraintDaysSeq(v, 1);
+
+	//Après 2 jours/nuits au moins 2 repos
+	v = vector<string>();
+	v.push_back("workL");
+	v.push_back("workL");
+	auto v2 = vector<string>();
+	v2.push_back("rest");
+	v2.push_back("rest");
+	ConstraintInvolved* cnr = new ConstraintInvolved(v, v2, 1);
+
+	//1 week ends par mois
+	v = vector<string>();
+	v.push_back("work");
+	v.push_back("work");
+	ConstraintSeqMinMax* cwe = new ConstraintSeqMinMax(Day::Saturday, MinMax::Min, 1, v, 1);
+
+	sdc->addConstraint(cJN);
+	sdc->addConstraint(c3N);
+	sdc->addConstraint(crn);
+	sdc->addConstraint(cnr);
+	sdc->addConstraint(cwe);
+
+
+	float nbHoursWeek = 48.0, nbHoursMonth = 155;
+
+	//Agents
+	Agent* a9 = new Agent("9", nbHoursMonth, nbHoursWeek, Status::Confirmed);
+	m.addAgent(a9, sdc);
+
+
+	Agent* a12 = new Agent("12", nbHoursMonth, nbHoursWeek, Status::Beginner);
+	a12->setService(sdc);
+	a12->setCalendarDay(fp, 23, true);
+	a12->setCalendarDay(ca, 29, true);
+	a12->setCalendarDay(ca, 30, true);
+	m.addAgent(a12, sdc);
+
+	Agent* a17 = new Agent("17", nbHoursMonth, nbHoursWeek, Status::Beginner);
+	a17->setService(sdc);
+	a17->setCalendarDay(ca, 3, true);
+	a17->setCalendarDay(ca, 5, true);
+	a17->setCalendarDay(ca, 10, true);
+	a17->setCalendarDay(ca, 12, true);
+	a17->setCalendarDay(ca, 17, true);
+	a17->setCalendarDay(ca, 24, true);
+	a17->setCalendarDay(ca, 26, true);
+	m.addAgent(a17, sdc);
+
+	Agent* a34 = new Agent("34", nbHoursMonth, nbHoursWeek, Status::Beginner);
+	a34->setService(sdc);
+	a34->setCalendarDay(fp, 1, true);
+	a34->setCalendarDay(fp, 2, true);
+	addConsecutiveSamePost(a34, ca, 3, 9);
+	m.addAgent(a34, sdc);
+
+	Agent* a39 = new Agent("39", nbHoursMonth, nbHoursWeek, Status::Confirmed);
+	a39->setService(sdc);
+	a39->setCalendarDay(ca, 27, true);
+	a39->setCalendarDay(ca, 28, true);
+	a39->setCalendarDay(ca, 29, true);
+	a39->setCalendarDay(ca, 30, true);
+	m.addAgent(a39, sdc);
+
+	Agent* a46 = new Agent("46", nbHoursMonth, nbHoursWeek, Status::Confirmed);
+	a46->setService(sdc);
+	a46->setCalendarDay(fp, 23, true);
+	addConsecutiveSamePost(a46, ca, 25, 30);
+	m.addAgent(a46, sdc);
+
+	Agent* a50 = new Agent("50", nbHoursMonth, nbHoursWeek, Status::Beginner);
+	a50->setService(sdc);
+	a50->setCalendarDay(fp, 23, true);
+	m.addAgent(a50, sdc);
+
+	Agent* a61 = new Agent("61", nbHoursMonth, nbHoursWeek, Status::Confirmed);
+	a61->setService(sdc);
+	a61->setCalendarDay(ca, 0, true);
+	addConsecutiveSamePost(a61, ca, 8, 14);
+	m.addAgent(a61, sdc);
+
+
+	Agent* a29 = new Agent("29", nbHoursMonth, nbHoursWeek, Status::Confirmed);
+	a29->setService(sdc);
+	m.addAgent(a29, sdc);
+
+	Agent* a59 = new Agent("59", nbHoursMonth, nbHoursWeek, Status::Beginner);
+	a59->setService(sdc);
+	a59->setCalendarDay(ca, 0, true);
+	a59->setCalendarDay(ca, 1, true);
+	addConsecutiveSamePost(a59, ca, 27, 30);
+	m.addAgent(a59, sdc);
+
+	Agent* a48 = new Agent("48", nbHoursMonth, nbHoursWeek, Status::Beginner);
+	a48->setService(sdc); 
+	m.addAgent(a48, sdc);
+
+	Agent* a15 = new Agent("15", nbHoursMonth, nbHoursWeek, Status::Confirmed);
+	a15->setService(sdc);
+	addConsecutiveSamePost(a15, ca, 0, 30);
+	m.addAgent(a15, sdc);
+
+	return m;
+
+}
+
 
 int main() {
 
 	srand(time(0));
 
 	Model m =  generateGhr();
-
+	m = addServiceSDC(m); //ajoute le service SDC au modèle
+	
 	//auto m2 = heuristicSolver::iterative(m,100,300,3);
 
 	auto m2 = Model::generateModelInstance(Day::Sunday, 31, 25, 2, 5, 10, 48.0, 155);
