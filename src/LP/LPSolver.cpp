3b36adf59073a325223ead521218deb89bdf730a
Anthylnem <anthea.richaume@gmail.com>
2020-05-27 15:08:28 +0200

    Contraintes suites de jours/nuits - nuit puis jour interdit - 3 nuits d'affilée interdit - 3 jours d'affilée possible en GHR, SDC et UK

--- src/LP/LPSolver.cpp
+++ src/LP/LPSolver.cpp
@@ -255,6 +255,323 @@ Model LPSolver::linearProgram(const Model mo) {
 		}
 	}
 
+	// Nuit puis Jour interdit
+	varsDataTmp = varsData; //variable temporaire pour la contrainte
+	for (int day = 0; day < mr.getNbDays(); day++) {
+		for (auto s : mr.getServices()) {
+			if (day < mr.getNbDays() - 1) {
+				for (auto a : agents) {
+
+					// Vérifier si l'agent ne travaille pas déjà de jour le jour suivant
+					bool workByDayAfter = false;
+					int d1 = day + 1;
+
+					auto work1 = a->getCalendar()[d1];
+					if (work1 != NULL) {
+						vector<string> att = work1->getAttributs();
+						if (count(att.begin(), att.end(), "day")) {
+							workByDayAfter = true;
+						}
+					}
+
+					if (a->getCalendar()[day] == NULL || workByDayAfter) {
+						for (auto nightJob : s->getPosts()) {
+
+							vector<string> attNight = nightJob->getAttributs();
+
+							if (count(attNight.begin(), attNight.end(), "night")) {
+
+								int v = 0;
+								SCIP_VAR* varCons[10];
+								SCIP_VAR* varNight;
+								for (int i = 0; i < varsDataTmp.size(); i++) {
+									if (varsDataTmp[i]->getAgent() == a && varsDataTmp[i]->getDay() == day && varsDataTmp[i]->getPost() == nightJob) {
+										varNight = varsDataTmp[i]->getVariable();
+										varCons[v] = varNight;
+										v++;
+										break;
+									}
+								}
+
+								// Jour suivant
+								for (auto dayJob : s->getPosts()) {
+									vector<string> attDay = dayJob->getAttributs();
+
+									if (count(attDay.begin(), attDay.end(), "day")) {
+
+										SCIP_VAR* varDay;
+										bool noVar = true;
+
+										for (int i = 0; i < varsDataTmp.size(); i++) {
+											// Poste jour suivant == NULL
+											if (varsDataTmp[i]->getAgent() == a && varsDataTmp[i]->getDay() == (day + 1) && varsDataTmp[i]->getPost() == dayJob && v == 1) {
+												varDay = varsDataTmp[i]->getVariable();
+												varCons[v] = varDay;
+												v++;
+												//cout << v << endl;
+												SCIP_CONS* cons;
+												SCIPcreateConsBasicSetpack(scip, &cons, "nuit_jour_interdit", v, varCons);
+												SCIPaddCons(scip, cons);
+
+												auto it = consVec.end();
+												consVec.insert(it, cons);
+
+												id_contrainte++;
+												noVar = false;
+
+												//Break si variable trouvée
+												break;
+											}
+										}
+
+										// Poste jour suivant != NULL (il n'existe pas de variable)
+										if (noVar && v == 1) {
+											SCIP_CONS* cons;
+											long long un[1];
+											un[0] = (long long)1;
+											SCIPcreateConsBasicKnapsack(scip, &cons, "nuit_jour_interdit", v, varCons, un, 0);
+											SCIPaddCons(scip, cons);
+
+											auto it = consVec.end();
+											consVec.insert(it, cons);
+
+											id_contrainte++;
+
+											//Break car contrainte finie pour nightJob et DayJob courant
+											break;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+
+			// 3 nuits d'affilée interdit
+			if (day < mr.getNbDays() - 2) {
+				for (auto a : agents) {
+					if (a->getCalendar()[day] == NULL) {
+						int nbVar = 0;
+						SCIP_VAR* varCons[10];
+						for (auto nightOne : s->getPosts()) {
+
+							vector<string> attNightOne = nightOne->getAttributs();
+
+							if (count(attNightOne.begin(), attNightOne.end(), "night")) {
+
+								SCIP_VAR* varNightOne;
+
+								for (int i = 0; i < varsDataTmp.size(); i++) {
+									if (varsDataTmp[i]->getAgent() == a && varsDataTmp[i]->getDay() == day && varsDataTmp[i]->getPost() == nightOne) {
+										//cout << varsDataTmp[i]->getDay() << " " << varsDataTmp[i]->getAgent()->getId() << endl;
+										varNightOne = varsDataTmp[i]->getVariable();
+										varCons[nbVar] = varNightOne;
+										nbVar++;
+										break;
+									}
+								}
+								int d1 = day + 1;
+								if (a->getCalendar()[d1] == NULL) {
+									for (auto nightTwo : s->getPosts()) {
+
+										vector<string> attNightTwo = nightTwo->getAttributs();
+
+										if (count(attNightTwo.begin(), attNightTwo.end(), "night")) {
+											SCIP_VAR* varNightTwo;
+
+											for (int i = 0; i < varsDataTmp.size(); i++) {
+												if (varsDataTmp[i]->getAgent() == a && varsDataTmp[i]->getDay() == d1 && varsDataTmp[i]->getPost() == nightTwo) {
+													//cout << varsDataTmp[i]->getDay() << " " << varsDataTmp[i]->getAgent()->getId() << endl;
+													varNightTwo = varsDataTmp[i]->getVariable();
+													varCons[nbVar] = varNightTwo;
+													nbVar++;
+													break;
+												}
+											}
+
+											int d2 = d1 + 1;
+											if (a->getCalendar()[d2] == NULL) {
+												for (auto nightThree : s->getPosts()) {
+
+													vector<string> attNightThree = nightThree->getAttributs();
+
+													if (count(attNightThree.begin(), attNightThree.end(), "night")) {
+														SCIP_VAR* varNightThree;
+
+														for (int i = 0; i < varsDataTmp.size(); i++) {
+															if (varsDataTmp[i]->getAgent() == a && varsDataTmp[i]->getDay() == d2 && varsDataTmp[i]->getPost() == nightThree) {
+																//cout << varsDataTmp[i]->getDay() << " " << varsDataTmp[i]->getAgent()->getId() << endl;
+																varNightThree = varsDataTmp[i]->getVariable();
+																varCons[nbVar] = varNightThree;
+																nbVar++;
+																break;
+															}
+														}
+
+														SCIP_CONS* cons;
+														long long un[3];
+														for (int i = 0; i < 3; i++) {
+															un[i] = (long long)1;
+														}
+
+														SCIPcreateConsBasicKnapsack(scip, &cons, "3_nuits_interdit", nbVar, varCons, un, 2);
+														SCIPaddCons(scip, cons);
+
+														auto it = consVec.end();
+														consVec.insert(it, cons);
+
+														id_contrainte++;
+
+														//Break car contrainte finie pour day, day+1, day+2
+														break;
+
+													}
+												}
+											}
+										}
+									}
+								}
+
+							}
+						}
+					}
+				}
+			}
+
+			// 3 jours d'affilée possible en GHR, SDC et UK
+			string idService = s->getId();
+			if (idService == "GHR" || idService == "SDC" || idService == "UK") {
+				if (day < mr.getNbDays() - 3 && ((day + 1) % 4 == 0 || day == 0)) {
+					for (auto a : agents) {
+						for (auto dayOne : s->getPosts()) {
+							int nbVar = 0;
+							SCIP_VAR* varCons[10];
+
+							if (nbVar >= 3) {
+								nbVar = 0;
+								SCIP_VAR* varCons[10];
+							}
+							vector<string> attDayOne = dayOne->getAttributs();
+
+							if (count(attDayOne.begin(), attDayOne.end(), "day")) {
+								SCIP_VAR* varDayOne;
+								for (int i = 0; i < varsDataTmp.size(); i++) {
+									if (varsDataTmp[i]->getAgent() == a && varsDataTmp[i]->getDay() == day && varsDataTmp[i]->getPost() == dayOne) {
+										//cout << varsDataTmp[i]->getDay() << " " << varsDataTmp[i]->getAgent()->getId() << " " << varsDataTmp[i]->getPost()->getId() << endl;
+										varDayOne = varsDataTmp[i]->getVariable();
+										varCons[nbVar] = varDayOne;
+										nbVar++;
+										break;
+									}
+								}
+
+								int d1 = day + 1;
+								if (a->getCalendar()[d1] == NULL) {
+									for (auto dayTwo : s->getPosts()) {
+
+										vector<string> attDayTwo = dayTwo->getAttributs();
+
+										if (count(attDayTwo.begin(), attDayTwo.end(), "day")) {
+											SCIP_VAR* varDayTwo;
+
+											if (nbVar >= 4) {
+												nbVar = 1;
+											}
+
+											for (int i = 0; i < varsDataTmp.size(); i++) {
+												if (varsDataTmp[i]->getAgent() == a && varsDataTmp[i]->getDay() == d1 && varsDataTmp[i]->getPost() == dayTwo) {
+													//cout << varsDataTmp[i]->getDay() << " " << varsDataTmp[i]->getAgent()->getId() << " " << varsDataTmp[i]->getPost()->getId() << endl;
+													varDayTwo = varsDataTmp[i]->getVariable();
+													varCons[nbVar] = varDayTwo;
+													nbVar++;
+													break;
+												}
+											}
+
+											int d2 = d1 + 1;
+											if (a->getCalendar()[d2] == NULL) {
+												for (auto dayThree : s->getPosts()) {
+
+													vector<string> attDayThree = dayThree->getAttributs();
+
+													if (count(attDayThree.begin(), attDayThree.end(), "day")) {
+														SCIP_VAR* varDayThree;
+
+														if (nbVar >= 4) {
+															nbVar = 2;
+														}
+
+														for (int i = 0; i < varsDataTmp.size(); i++) {
+															if (varsDataTmp[i]->getAgent() == a && varsDataTmp[i]->getDay() == d2 && varsDataTmp[i]->getPost() == dayThree) {
+																//cout << varsDataTmp[i]->getDay() << " " << varsDataTmp[i]->getAgent()->getId() << " " << varsDataTmp[i]->getPost()->getId() << endl;
+																varDayThree = varsDataTmp[i]->getVariable();
+																varCons[nbVar] = varDayThree;
+																nbVar++;
+																break;
+															}
+														}
+														//cout << a->getId() << " " << day << " " << dayOne->getId() << " " << dayTwo->getId() << " " << dayThree->getId() << " " << nbVar << endl;
+
+														int d3 = d2 + 1;
+														if (a->getCalendar()[d3] == NULL) {
+															for (auto dayFour : s->getPosts()) {
+
+																vector<string> attDayFour = dayFour->getAttributs();
+
+																if (count(attDayFour.begin(), attDayFour.end(), "day")) {
+																	SCIP_VAR* varDayFour;
+
+																	if (nbVar >= 4) {
+																		nbVar = 3;
+																	}
+
+																	for (int i = 0; i < varsDataTmp.size(); i++) {
+																		if (varsDataTmp[i]->getAgent() == a && varsDataTmp[i]->getDay() == d3 && varsDataTmp[i]->getPost() == dayFour) {
+																			//cout << varsDataTmp[i]->getDay() << " " << varsDataTmp[i]->getAgent()->getId() << " " << varsDataTmp[i]->getPost()->getId() << endl;
+																			varDayFour = varsDataTmp[i]->getVariable();
+																			varCons[nbVar] = varDayFour;
+																			nbVar++;
+																			break;
+																		}
+																	}
+
+																	if (nbVar == 4) {
+																		SCIP_CONS* cons;
+																		long long un[4];
+																		for (int i = 0; i < 4; i++) {
+																			un[i] = (long long)1;
+																		}
+
+
+																		SCIPcreateConsBasicKnapsack(scip, &cons, "3_jours_exceptionnel", nbVar, varCons, un, 3);
+																		SCIPaddCons(scip, cons);
+
+																		auto it = consVec.end();
+																		consVec.insert(it, cons);
+
+																		id_contrainte++;
+
+																		//Break car contrainte finie pour day, day+1, day+2
+																		//break;
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
 
 	fileIdVar.close();
 
